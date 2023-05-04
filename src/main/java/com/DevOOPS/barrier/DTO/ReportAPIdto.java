48deba7ec89ca04bbd143573773d015c998d64a6
CDoGH <sa03048@gmail.com>
2023-05-05 02:24:53 +0900

    특보 API 수정 230504 1. String token으로 특보명 나눔. 2. Mapper 추가. 특보 명 나눠서 들어가는 것 확인. 3. DB 매개변수 확인

--- src/main/java/com/DevOOPS/barrier/DTO/ReportAPIdto.java
+++ src/main/java/com/DevOOPS/barrier/DTO/ReportAPIdto.java
@@ -7,19 +7,20 @@ import java.util.Date;
 @Data
 public class ReportAPIdto { //front.
     private int stnId;
-    private String title;
     private Date tmFc; //날짜
     private int tmSeq;
-
     private String regionData;
+    private String title1;
+    private String title2;
 
-    public ReportAPIdto(int stnId, String title, Date tmFc,
-                        int tmSeq, String regionData ) {
+    public ReportAPIdto(int stnId, Date tmFc,
+                        int tmSeq, String regionData, String title1, String title2 ) {
         this.stnId = stnId;
-        this.title = title;
         this.tmFc = tmFc;
         this.tmSeq = tmSeq;
         this.regionData = regionData;
+        this.title1 = title1;
+        this.title2 = title2;
     }
 
 
