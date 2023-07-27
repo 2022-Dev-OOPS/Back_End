7ee590e034b2db215e649ee2ee6de32891ba5377
CDoGH <sa03048@gmail.com>
2023-07-27 09:34:06 +0900

    refactor: EnableAsync Annotation

--- src/main/java/com/DevOOPS/barrier/BarrierApplication.java
+++ src/main/java/com/DevOOPS/barrier/BarrierApplication.java
@@ -3,9 +3,11 @@ package com.DevOOPS.barrier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class BarrierApplication {
 
